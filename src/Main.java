08143c8922362ece3541572591ec4d6ced3d5302
HyeGyung <gyung688@gmail.com>
2021-04-11 23:52:36 +0900

    이것은 Main 파일입니다.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		
+		//깃허브 올리기
+		/*
+		1. cmd 창을 연다.
+			git init //git repository 만들기
+		*/
+	}
+
+}
